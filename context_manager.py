453f78edf7ba3011023d79add16ff35481cb6459
Vladimir Eroshenko <admin@it-vi.ru>
2019-06-30 20:45:52 +0300

    release 1.0

---
+++ context_manager.py
@@ -0,0 +1,39 @@
+import subprocess
+from datetime import datetime
+# import logging
+# import sys
+# sys.stdout = open('test_0.log', 'a')
+
+
+time_1 = datetime.now()
+
+# I am using linux. Only Linux. Sorry))
+# Команда трассировки. Хост it-vi.ru лично мой, хотя трассировку можно запускать совершенно к любому узлу в интернетах))
+cmd = '/bin/mtr -4 -o "LS DR AGJMXI NBWV" -zrc 50 it-vi.ru >> test.log'
+
+# cmd2 = ['/bin/mtr', '-o', 'LS DR AGJMXI NBWV', '-zrc', '5', 'it-vi.ru', '>>', 'test.log']
+
+class Mytraceroute:
+    def __init__(self, cmd):
+        self.time_cm_1 = datetime.now()
+        print(f'\n=== Запуск менеджера контекста ==='
+              f'\n=== {self.time_cm_1} ===\n')
+        self.proc = subprocess.Popen(cmd, shell=True)
+
+    def __enter__(self):
+        return self.proc
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.proc.kill()
+        self.time_cm_2 = datetime.now()
+        print(f'\n=== Конец менеджера контекста ==='
+              f'\n=== {self.time_cm_2} ===')
+        print(f'\n*** Менеджер контекста выполнился за {(self.time_cm_2 - self.time_cm_1).total_seconds()} секунд. ***\n')
+
+
+
+with Mytraceroute(cmd) as shell:
+    shell.communicate()
+
+time_2 = datetime.now()
+print(f'\n>>> Полностью вся программа выполнилась за {(time_2 - time_1).total_seconds()} секунд. <<<\n'.upper())
